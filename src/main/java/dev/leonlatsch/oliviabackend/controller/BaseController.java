6e14f76bfdd2c5c5c967db841323d9374b796021
Leon Latsch <dev.leonlatsch@gmail.com>
2019-11-26 11:50:54 +0100

    added get broker port

--- src/main/java/dev/leonlatsch/oliviabackend/controller/BaseController.java
+++ src/main/java/dev/leonlatsch/oliviabackend/controller/BaseController.java
@@ -3,6 +3,8 @@ package dev.leonlatsch.oliviabackend.controller;
 import dev.leonlatsch.oliviabackend.constants.CommonResponses;
 import dev.leonlatsch.oliviabackend.dto.Response;
 import dev.leonlatsch.oliviabackend.util.MavenProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +15,9 @@ import static dev.leonlatsch.oliviabackend.util.ControllerUtils.createResponseEn
 @RestController
 @RequestMapping("/")
 public class BaseController {
+
+    @Autowired
+    private Environment env;
 
     @RequestMapping(value = "healthcheck", method = RequestMethod.GET)
     public ResponseEntity<Response> healthCheck() {
@@ -31,5 +36,10 @@ public class BaseController {
     @RequestMapping(value = "version", method = RequestMethod.GET)
     public String version() {
         return MavenProperties.getVersion();
+    }
+
+    @RequestMapping(value = "broker-port", method = RequestMethod.GET)
+    public String brokerPort() {
+        return env.getProperty("spring.rabbitmq.port");
     }
 }
