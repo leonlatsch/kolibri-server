801724d47e8b780e322fb61636daeb3e06353f48
Leon Latsch <dev.leonlatsch@gmail.com>
2020-01-22 18:31:42 +0100

    added spring security

---
+++ src/main/java/dev/leonlatsch/oliviabackend/config/SpringSecurityConfig.java
@@ -0,0 +1,33 @@
+package dev.leonlatsch.oliviabackend.config;
+
+import com.sun.corba.se.impl.orb.ORBConfiguratorImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("*")
+                .authenticated()
+                .anyRequest()
+                .authenticated();
+    }
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
