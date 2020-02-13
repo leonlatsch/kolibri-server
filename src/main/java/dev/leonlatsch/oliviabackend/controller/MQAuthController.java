b2118cbce4e08912bbd88ac820cd3a1bdc61ea61
Leon Latsch <dev.leonlatsch@gmail.com>
2020-02-13 12:11:25 +0100

    added internal auth back in for test usage and to ensure the admin can log into rabbitmq even when the api is down

--- src/main/java/dev/leonlatsch/oliviabackend/controller/MQAuthController.java
+++ src/main/java/dev/leonlatsch/oliviabackend/controller/MQAuthController.java
@@ -3,8 +3,12 @@ package dev.leonlatsch.oliviabackend.controller;
 import dev.leonlatsch.oliviabackend.model.dto.rabbitmq.ResourceCheck;
 import dev.leonlatsch.oliviabackend.model.dto.rabbitmq.VirtualHostCheck;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+
+import javax.naming.Context;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -15,17 +19,9 @@ public class MQAuthController {
     private static final String ALLOW_ADMIN = "allow administrator";
     private static final String DENY = "deny";
 
-    @Autowired
-    private Environment env;
-
     @RequestMapping(value = "user", method = RequestMethod.POST)
     public String user(@RequestParam("username") String username, @RequestParam("password") String password) {
-        String internalAdminUsername = env.getProperty("spring.rabbitmq.username");
-        String internalAdminPassword = env.getProperty("spring.rabbitmq.password");
-        if (username != null && password != null && username.equals(internalAdminUsername ) && password.equals(internalAdminPassword)) {
-            return ALLOW_ADMIN;
-        }
-        return ALLOW;
+        return DENY;
     }
 
     @RequestMapping(value = "vhost", method = RequestMethod.POST)
@@ -37,6 +33,7 @@ public class MQAuthController {
     public String resource(ResourceCheck check) {
         return ALLOW;
     }
+
     @RequestMapping(value = "topic", method = RequestMethod.POST)
     public String topic() {
         return ALLOW;
